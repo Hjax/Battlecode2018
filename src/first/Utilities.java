c27c0c2582d8674e5dfbc9fb73a29629b96f7898
quasarprintf <pikachar@hotmail.com>
2018-01-11 11:12:07 -0500

    Utilities and Build order turn 1
    I thought I already committed utilities...

--- src/first/Utilities.java
+++ src/first/Utilities.java
@@ -1,5 +1,56 @@
 package first;
 
-public class Utilities {
+import bc.*;
 
+public class Utilities 
+{
+	public static Direction oppositeDir(Direction dir)
+	{
+		switch (dir)
+		{
+		case East:
+			return Direction.West;
+		case West:
+			return Direction.East;
+		case North:
+			return Direction.South;
+		case South:
+			return Direction.North;
+		case Northeast:
+			return Direction.Southwest;
+		case Northwest:
+			return Direction.Southeast;
+		case Southeast:
+			return Direction.Northwest;
+		case Southwest:
+			return Direction.Northeast;
+		default:
+			return Direction.Center;
+		}
+	}
+	
+	public static MapLocation offsetInDirection(MapLocation start, Direction dir, int offset)
+	{
+		switch (dir)
+		{
+		case East:
+			return new MapLocation(start.getPlanet(), start.getX() + offset, start.getY());
+		case West:
+			return new MapLocation(start.getPlanet(), start.getX() - offset, start.getY());
+		case North:
+			return new MapLocation(start.getPlanet(), start.getX(), start.getY() + offset);
+		case South:
+			return new MapLocation(start.getPlanet(), start.getX(), start.getY() - offset);
+		case Northeast:
+			return new MapLocation(start.getPlanet(), start.getX() + offset, start.getY() + offset);
+		case Northwest:
+			return new MapLocation(start.getPlanet(), start.getX() - offset, start.getY() + offset);
+		case Southeast:
+			return new MapLocation(start.getPlanet(), start.getX() + offset, start.getY() - offset);
+		case Southwest:
+			return new MapLocation(start.getPlanet(), start.getX() - offset, start.getY() - offset);
+		default:
+			return start;
+		}
+	}
 }
--- src/first/Worker.java
+++ src/first/Worker.java
@@ -1,9 +1,13 @@
 package first;
 
+import java.util.HashSet;
+
 import bc.*;
 
 public class Worker 
 {
+	
+	private static HashSet<Robot> currentBlueprints = new HashSet<Robot>();
 	
 	private static class WorkerDistanceTuple
 	{
@@ -22,8 +26,8 @@ public class Worker
 		int min = Constants.INFINITY;
 		int max = -1;
 		int pathDistance = -1;
-		WorkerDistanceTuple closeWorker = new WorkerDistanceTuple(-1, null);
-		WorkerDistanceTuple farWorker = new WorkerDistanceTuple(Constants.INFINITY, null);
+		WorkerDistanceTuple closeWorker = new WorkerDistanceTuple(Constants.INFINITY, null);
+		WorkerDistanceTuple farWorker = new WorkerDistanceTuple(-1, null);
 		
 		for (int ally = 0; ally < Constants.startingAlliesLocation.length; ally++)
 		{
@@ -41,29 +45,60 @@ public class Worker
 			}
 			if (max < closeWorker.rushDistance)
 			{
+				System.out.printf("Updating closeWorker\n");
 				closeWorker.rushDistance = max;
 				closeWorker.worker = Constants.startingAllies[ally];
 			}
 			if (min > farWorker.rushDistance)
 			{
-				
+				System.out.printf("Updating farWorker\n");
 				farWorker.rushDistance = min;
 				farWorker.worker = Constants.startingAllies[ally];
 			}
 		}
-		if (closeWorker.rushDistance < Constants.RUSHTHRESHOLD)
+		if (closeWorker.rushDistance <= Constants.RUSHTHRESHOLD)
 		{
+			System.out.printf("rush distance is %d\tworker id is %d\n", closeWorker.rushDistance, closeWorker.worker.id());
 			return closeWorker;
 		}
+		System.out.printf("rush distance is %d\tworker id is %d\n", farWorker.rushDistance, farWorker.worker.id());
 		return farWorker;
 	}
 	
 	
+	private static void buildOrder()
+	{
+		if (Game.round() == 1)
+		{
+			WorkerDistanceTuple bestWorker = bestWorker();
+			MapLocation nearestEnemy = Constants.startingEnemiesLocation[0];
+
+			for (MapLocation enemy:Constants.startingEnemiesLocation)
+			{
+				if (Pathfinding.pathLength(bestWorker.worker.mapLocation(), enemy) < Pathfinding.pathLength(bestWorker.worker.mapLocation(), nearestEnemy))
+				{
+					nearestEnemy = enemy;
+				}
+			}
+			Direction buildDir = Pathfinding.path(bestWorker.worker.mapLocation(), nearestEnemy);;
+			if (bestWorker.rushDistance > Constants.RUSHTHRESHOLD)
+			{
+				buildDir = Utilities.oppositeDir(buildDir);
+			}
+			System.out.printf("bestWorker id is %d\n", bestWorker.worker.id());
+			Game.blueprint(bestWorker.worker, UnitType.Factory, buildDir);
+			currentBlueprints.add(Game.senseNearbyUnits(Utilities.offsetInDirection(bestWorker.worker.mapLocation(), buildDir, 1), UnitType.Factory)[0]);
+		}
+	}
+	
+	
+	
 	public static void run() 
 	{
-		WorkerDistanceTuple bestWorker = bestWorker();
-		WorkerDistanceTuple nearestEnemy = new WorkerDistanceTuple(Constants.INFINITY, null);
-
+		if (Game.planet() == Planet.Earth)
+		{
+			buildOrder();
+		}
 		
 	}
 }
