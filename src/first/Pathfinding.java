10f3d50db3b4f85f39703a9880640768aa9ff379
Kevin Konrad <Kevintkonrad@gmail.com>
2018-01-11 20:10:15 -0500

    pathfinding improvements

--- src/first/Pathfinding.java
+++ src/first/Pathfinding.java
@@ -5,45 +5,58 @@ import java.util.*;
 
 public class Pathfinding {
 	private static Map<Tile, Map<Tile, Integer>> cache;
+	private static Map<Tile, Queue<Tile>> open;
+	private static Map<Tile, Set<Tile>> closed;
 	static {
 		cache = new HashMap<>();
+		open = new HashMap<>();
+		closed = new HashMap<>();
 	}
 	
-	private static void bfs(Tile dest) {
+	private static void bfs(Tile source, Tile dest) {
 
-		 Map<Tile, Integer> current_map = new HashMap<>();
-		 Queue<Tile> open = new LinkedList<>();
-		 Set<Tile> closed = new HashSet<>();
+		 if (!cache.containsKey(dest)) {
+			 open.put(dest, new LinkedList<>());
+			 closed.put(dest, new HashSet<>());
+			 cache.put(dest, new HashMap<>());
+			 
+		 }
 		 
-		 open.add(dest);
-		 closed.add(dest);
-		 current_map.put(dest, 0);
-		 while (open.size() > 0) {
-			Tile current = open.poll();
+		 open.get(dest).add(dest);
+		 closed.get(dest).add(dest);
+		 cache.get(dest).put(dest, 0);
+
+		 while (open.get(dest).size() > 0) {
+			Tile current = open.get(dest).poll();
 			// for each direction 
 			for (int i = 0; i < 8; i++) {
 				Tile test = current.add(Game.moveDirections[i]);
-				if (!closed.contains(test) && Game.isPassableTerrainAt(test)) {
-					open.add(test);
-					closed.add(test);
-					current_map.put(test, current_map.get(current) + 1);
+				if (!closed.get(dest).contains(test) && Game.isPassableTerrainAt(test)) {
+					open.get(dest).add(test);
+					closed.get(dest).add(test);
+					cache.get(dest).put(test, cache.get(dest).get(current) + 1);
 				}
 			}
+			if (current == source) {
+				return;
+			}
 		 }
-		 cache.put(dest, current_map);
 	}
 	
 	public static Direction path(Tile source, Tile dest) {
-		if (cache.containsKey(dest)) {
-			bfs(dest);
+		if (!(cache.containsKey(dest) && cache.get(dest).containsKey(source))) {
+			bfs(source, dest);
 		}
 		if (!cache.get(dest).containsKey(source)) {
 			return Direction.Center;
 		}
 		Direction best = Direction.Center;
 		for (Direction direction: Game.directions) {
-			if (Game.isPassableTerrainAt(source.add(direction))) {
-				if (cache.get(dest).get(source.add(best)) > cache.get(dest).get(source.add(direction))) {
+			if (Game.isPassableTerrainAt(source.add(direction)) && Game.isOccupiable(source.add(direction)) > 0) {
+				if (best == Direction.Center && (cache.get(dest).get(source.add(best)) >= cache.get(dest).get(source.add(direction)))) {
+					best = direction;
+				}
+				else if (cache.get(dest).get(source.add(best)) > cache.get(dest).get(source.add(direction))) {
 					best = direction;
 				}
 			}
@@ -52,10 +65,8 @@ public class Pathfinding {
 	}
 	
 	public static int pathLength(Tile source, Tile dest) {
-		if (!cache.containsKey(dest) && !cache.containsKey(source)) {
-			long start = System.nanoTime();
-			bfs(dest);
-			System.out.println((System.nanoTime() - start) / 1000000.0);
+		if (!(cache.containsKey(dest) && cache.get(dest).containsKey(source)) || !(cache.containsKey(source) && cache.get(source).containsKey(dest))) {
+			bfs(source, dest);
 		}
 		if (cache.containsKey(dest)) {
 			if (!cache.get(dest).containsKey(source)) {
